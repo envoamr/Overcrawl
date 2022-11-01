124df8c1ba508923490b764d21e0804d78d41321
r13Dm <ToothPaste32@outlook.com>
2022-11-01 14:56:07 -0400

    started work on webcrawling simulation interface

--- web/request.go
+++ web/request.go
@@ -1,1 +1,22 @@
 package web
+
+import (
+	"os"
+	"github.com/envoamr/Overcrawl/misc"
+)
+
+var website string
+
+func SendMeta(websiteName string) string {
+	web, err := os.ReadFile(websiteName)
+	website = string(web)
+
+	if err != nil {
+		panic(err)
+	}
+
+	startEnd := misc.Rand(2)
+	var start int = int(startEnd[0])
+	var off int = int(startEnd[1])
+	return website[start:start+off]
+}
