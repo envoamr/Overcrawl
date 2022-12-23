35a5317d2d653515e0fddb8983e9dd40313068ec
envoamr <66641746+envoamr@users.noreply.github.com>
2022-12-23 16:32:20 -0500

    modified to a dynamic path

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
-//"github.com/envoamr/Overcrawl/thread"
+	//"github.com/envoamr/Overcrawl/thread"
+	"os"
+
 	"github.com/envoamr/Overcrawl/web"
 )
 
 func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(dir)
 	for i := 0; i <= 100; i++ {
-		fmt.Println(web.SendMeta("/home/crest/repos/Overcrawl/sample/txt"))
+		fmt.Println(web.SendMeta(dir + "/sample/txt"));
 	}
 }
